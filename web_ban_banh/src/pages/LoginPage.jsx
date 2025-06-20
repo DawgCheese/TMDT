cc0cf3e4554a8bbefa59546d3a1556990f576c51
ptqani <20130374@st.hcmuaf.edu.vn>
2025-06-20 12:57:13 +0700

    fix loi

--- web_ban_banh/src/pages/LoginPage.jsx
+++ web_ban_banh/src/pages/LoginPage.jsx
@@ -32,7 +32,10 @@ const LoginPage = () => {
     setErrorMsg("");
 
     try {
-      const res = await axios.post("http://localhost:8080/api/auth/login", formData);
+     const res = await axios.post("http://localhost:8080/api/auth/login", formData, {
+       withCredentials: true, // ✅ để gửi và lưu cookie JSESSIONID
+     });
+
       const { username, token } = res.data;
 
       // Lưu thông tin vào localStorage
